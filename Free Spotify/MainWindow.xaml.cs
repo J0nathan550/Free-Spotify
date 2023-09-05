ab019e79693b5ac5140ff4d09b53ce04ba180810
J0nathan550 <ihudokormov@gmail.com>
2023-09-06 00:29:24 +0200

    Add project files.

---
+++ Free Spotify/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Free_Spotify
+{
+    public sealed partial class MainWindow : Page
+    {
+        public MainWindow()
+        {
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            Window.Current.SetTitleBar(BackgroundElement);
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.BackgroundColor = Windows.UI.Color.FromArgb(0, 0, 0, 0);
+            titleBar.ForegroundColor = Windows.UI.Color.FromArgb(255, 255, 255, 255);
+            titleBar.InactiveBackgroundColor = Windows.UI.Color.FromArgb(0, 0, 0, 0);
+            titleBar.InactiveForegroundColor = Windows.UI.Color.FromArgb(255, 255, 255, 255);
+            titleBar.ButtonBackgroundColor = Windows.UI.Color.FromArgb(0, 0, 0, 0);
+            titleBar.ButtonForegroundColor = Windows.UI.Color.FromArgb(255, 255, 255, 255);
+            titleBar.ButtonHoverBackgroundColor = Windows.UI.Color.FromArgb(255, 255, 255, 255);
+            titleBar.ButtonPressedBackgroundColor = Windows.UI.Color.FromArgb(255, 255, 255, 255);
+            titleBar.ButtonInactiveBackgroundColor = Windows.UI.Color.FromArgb(0, 0, 0, 0);
+            titleBar.ButtonInactiveForegroundColor = Windows.UI.Color.FromArgb(255, 255, 255, 255);
+            this.InitializeComponent();
+        }
+    }
+}
