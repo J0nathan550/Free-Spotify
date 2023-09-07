91ee88f2569a73ee301bed1eb3e0032f2367efe7
J0nathan550 <ihudokormov@gmail.com>
2023-09-07 20:57:20 +0200

    Managed to fetch full songs from spotify

--- Spotify Fetch/MainWindow.xaml.cs
+++ Spotify Fetch/MainWindow.xaml.cs
@@ -8,12 +8,15 @@ using System.Windows.Media;
 using NAudio.Wave;
 using SpotifyExplode;
 using SpotifyExplode.Tracks;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams ;
 
 namespace Spotify_Fetch
 {
     public partial class MainWindow : Window
     {
         public SpotifyClient SpotifyFreeClient = new SpotifyClient();
+        private YoutubeClient youtube;
 
         public MainWindow()
         {
@@ -22,10 +25,13 @@ namespace Spotify_Fetch
         }
 
         private Track track;
+        private string YouTubeID;
 
         private async void LoadingTracks()
         {
-            track = await SpotifyFreeClient.Tracks.GetAsync("https://open.spotify.com/track/5f6XpCkieq8sbwqvBrqIlN?si=2a53db6377f34c4d");
+            track = await SpotifyFreeClient.Tracks.GetAsync("https://open.spotify.com/track/7rhOaEGYyPzY81e7acou1k?si=23b434cff5274100");
+            YouTubeID = await SpotifyFreeClient.Tracks.GetYoutubeIdAsync("https://open.spotify.com/track/7rhOaEGYyPzY81e7acou1k?si=23b434cff5274100");
+
             TextBlock title = new TextBlock();
             title.Text = track.Title;
             title.Foreground = new SolidColorBrush(Color.FromArgb(255, 0,0 ,0));
@@ -38,44 +44,34 @@ namespace Spotify_Fetch
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            PlayMp3FromUrl(track.PreviewUrl);
+            PlayMp3FromUrl(YouTubeID);
         }
 
-        public async static void PlayMp3FromUrl(string url)
+        public async void PlayMp3FromUrl(string url)
         {
             await Task.Run(() =>
             {
-                using (Stream ms = new MemoryStream())
+                var youtube = new YoutubeClient();
+                var video = youtube.Videos.GetAsync($"https://youtube.com/watch?v={YouTubeID}");
+                var streamManifest = youtube.Videos.Streams.GetManifestAsync($"https://youtube.com/watch?v={YouTubeID}");
+                var streamInfo = streamManifest.Result.GetAudioStreams().GetWithHighestBitrate();
+                using (WaveStream blockAlignedStream =
+                    new BlockAlignReductionStream(
+                        WaveFormatConversionStream.CreatePcmStream(
+                            new MediaFoundationReader(streamInfo.Url))))
                 {
-                    using (Stream stream = WebRequest.Create(url)
-                        .GetResponse().GetResponseStream())
+                    using (WaveOut waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
                     {
-                        byte[] buffer = new byte[32768];
-                        int read;
-                        while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                        waveOut.Volume = 0.05f;
+                        waveOut.Init(blockAlignedStream);
+                        waveOut.Play();
+                        while (waveOut.PlaybackState == PlaybackState.Playing)
                         {
-                            ms.Write(buffer, 0, read);
-                        }
-                    }
-
-                    ms.Position = 0;
-                    using (WaveStream blockAlignedStream =
-                        new BlockAlignReductionStream(
-                            WaveFormatConversionStream.CreatePcmStream(
-                                new Mp3FileReader(ms))))
-                    {
-                        using (WaveOut waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
-                        {
-                            waveOut.Volume = 0.05f;
-                            waveOut.Init(blockAlignedStream);
-                            waveOut.Play();
-                            while (waveOut.PlaybackState == PlaybackState.Playing)
-                            {
-                                Thread.Sleep(100);
-                            }
+                            Thread.Sleep(100);
                         }
                     }
                 }
+
             });
         }
     }
